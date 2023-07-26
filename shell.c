2f1ab13cc3da8eacb120b98a1513471ec11f5cf1
alvnka <alvondu@gmail.com>
2023-07-26 18:36:23 +0300

    initial commit for shell.c

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - shell entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char cmd[100], command[100], *parameters[20];
+	int status;
+
+	while (1)
+	{
+		type_prompt();
+		read_command_1(cmd, parameters);
+		if (check_exit(cmd))
+			break;
+		if (fork() != 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			handle_command_1(cmd, parameters, NULL);
+		}
+	}
+	return (0);
+}
